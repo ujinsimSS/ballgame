8a9a849b7874b02cd584e9028ac1d0813d3d508b
‘ujinsimSS’ <syj11243@naver.com>
2024-04-01 17:50:50 +0900

    fix 숫자 범위

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
     if (colorIndex !== null) {
       const timer = setTimeout(() => {
         setCurrentColor(colors[colorIndex]);
-        const randomNumber = Math.floor(Math.random() * 30) + 1;
+        const randomNumber = Math.floor(Math.random() * 29) + 1;
         setNumber(randomNumber);
         setShowResult(true);
         setIsButtonDisabled(false);
